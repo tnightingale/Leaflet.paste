bf51c8df7a267372772975ad8b8bdef76ea16f04
Tom Nightingale <tom@affinitybridge.com>
2012-10-10 14:32:13 -0700

    updated license and copyright

--- grunt.js
+++ grunt.js
@@ -11,7 +11,20 @@ module.exports = function(grunt) {
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
         '* http://github.com/thegreat/Leaflet.paste/\n' +
         '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
-        'Tom Nightingale; Licensed MIT */'
+        'Affinity Bridge; Licensed GPLv3\n' +
+        '*\n' +
+        '* This program is free software: you can redistribute it and/or modify\n' +
+        '* it under the terms of the GNU General Public License as published by\n' +
+        '* the Free Software Foundation, either version 3 of the License, or\n' +
+        '* (at your option) any later version.\n' +
+        '*\n' +
+        '* This program is distributed in the hope that it will be useful,\n' +
+        '* but WITHOUT ANY WARRANTY; without even the implied warranty of\n' +
+        '* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n' +
+        '* GNU General Public License for more details.\n' +
+        '*\n' +
+        '* You should have received a copy of the GNU General Public License\n' +
+        '* along with this program.  If not, see <http://www.gnu.org/licenses/>. */\n'
     },
     lint: {
       files: ['grunt.js', 'js/**/*.js', 'test/**/*.js']
